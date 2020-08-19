19aaed7c64efa2219779d2b28a9f276076accd0c
mitch reed <reed.mitch@live.com>
2020-08-19 13:13:03 -0400

    Updates

---
+++ precache-manifest.053db71af5529bbab276b5a4ac254a7c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15d00ae60b06f2ce84fc33eb91d940b8",
+    "url": "/NewPortfolio/index.html"
+  },
+  {
+    "revision": "87ba35b1d87de7e7153b",
+    "url": "/NewPortfolio/static/css/main.430211b1.chunk.css"
+  },
+  {
+    "revision": "3ba50b34f4026b642ad1",
+    "url": "/NewPortfolio/static/js/2.3fdd1dea.chunk.js"
+  },
+  {
+    "revision": "4386012a6d334684338618496b7aac20",
+    "url": "/NewPortfolio/static/js/2.3fdd1dea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87ba35b1d87de7e7153b",
+    "url": "/NewPortfolio/static/js/main.369b0154.chunk.js"
+  },
+  {
+    "revision": "4b8919ee7c9518ae1d38",
+    "url": "/NewPortfolio/static/js/runtime-main.e3d0d84a.js"
+  },
+  {
+    "revision": "18173e11af5922e1c58c3df3dd129a01",
+    "url": "/NewPortfolio/static/media/APIProj.18173e11.png"
+  },
+  {
+    "revision": "a2689606673c4de2adc6b74588884364",
+    "url": "/NewPortfolio/static/media/GigHubCreate.a2689606.jpg"
+  },
+  {
+    "revision": "efdba04681a3215e786c0e996e4bc780",
+    "url": "/NewPortfolio/static/media/header-background.efdba046.jpg"
+  },
+  {
+    "revision": "e377a025df8ca96aa8c7ee1817400c38",
+    "url": "/NewPortfolio/static/media/investPic.e377a025.png"
+  },
+  {
+    "revision": "ddd061567264a6e752abe5228ffea085",
+    "url": "/NewPortfolio/static/media/lottoApp.ddd06156.png"
+  },
+  {
+    "revision": "48f60d30f319d5ca1d47c265b20c02d0",
+    "url": "/NewPortfolio/static/media/moviePortfolio.48f60d30.png"
+  },
+  {
+    "revision": "7829a02aa276a510a01133587a03d701",
+    "url": "/NewPortfolio/static/media/testimonials-bg.7829a02a.jpg"
+  }
+]);
